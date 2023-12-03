1fe18f44740936a50b53dab8b16e8ac5718213bf
Lumen Lohn <hi@lumen.pink>
2023-12-03 16:41:35 +0000

    Check and install rust before if not

--- src/wasm-pack/install.sh
+++ src/wasm-pack/install.sh
@@ -3,5 +3,10 @@ set -e
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 #
+if ! which rustup > /dev/null; then
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
