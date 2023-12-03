493dd1aabc17870ce1527831c036d2b6abebf7d3
Lumen Lohn <hi@lumen.pink>
2023-12-03 20:51:16 +0000

    fix install to usr/local/bin

--- src/wasm-pack/install.sh
+++ src/wasm-pack/install.sh
@@ -7,7 +7,8 @@ which curl > /dev/null || (apt update && apt install curl -y -qq)
 if ! which rustup > /dev/null; then   
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     source $HOME/.cargo/env
-    export PATH=$PATH:$HOME/.cargo/bin 
 fi
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+cp -a $HOME/.cargo/bin/wasm-pack /usr/local/bin
