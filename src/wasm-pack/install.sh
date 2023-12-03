88037692f2a23894b5b6342da29e0ed025cb42ec
Lumen Lohn <hi@lumen.pink>
2023-12-03 16:47:55 +0000

    fix script

--- src/wasm-pack/install.sh
+++ src/wasm-pack/install.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 #
-if ! which rustup > /dev/null; then
-    which curl > /dev/null || (apt update && apt install curl -y -qq)
+which curl > /dev/null || (apt update && apt install curl -y -qq)
+if ! which rustup > /dev/null; then   
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     source $HOME/.cargo/env
+    export PATH=$PATH:$HOME/.cargo/bin 
 fi
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
